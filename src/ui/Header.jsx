b0a5e49eb5798b0c00d5b10ed6053de03b48a972
petershaan12 <2181002@unai.edu>
2024-01-19 07:02:27 +0700

    mengganti nama

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/username";
+import Username from '../features/user/Username';
 
 function Header() {
   return (
